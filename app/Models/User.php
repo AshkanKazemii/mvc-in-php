6265372f4ec29f669db27f85d730bd64f5eb99bf
ashkan kazemi <ashkan.kazemi.pln@gmail.com>
2023-01-29 15:37:07 +0330

    update models

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,9 @@
 
 namespace App\Models ;
 
-use App\Models\Tables\UserTable;
 
 class User
 {
-    public static $instance = null ;
-
-    private function __construct(){}
-
-    private static function getInstance()
-    {
-        if(static::$instance === null){
-            static::$instance = new UserTable() ;
-        }
-
-        return static::$instance; 
-    }
-
-    public static function __callStatic($name, $arguments)
-    {
-        return static::getFacadeAccessore()->$name() ;   
-    }
-
-    private static function getFacadeAccessore()
-    {
-        return static::getInstance();
-    }
+    
 
 }
